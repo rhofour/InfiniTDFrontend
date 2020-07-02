bfddb4e9c853f7d61e114a68187344a26a2b2616
R. Alex Hofer <me@rofer.me>
2020-07-02 15:38:05 -0400

    Added incredibly simple login ability.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginErrorMsg: string = "";
 
-  constructor() { }
+  constructor(
+    public afAuth: AngularFireAuth
+  ) { }
+
+  loginError(err: auth.Error) {
+    console.log(err);
+    this.loginErrorMsg = "Login error: " + err.message;
+  }
+
+  googleLogin() {
+    this.afAuth.signInWithPopup(new auth.GoogleAuthProvider()).catch(this.loginError);
+  }
+
+  logout() {
+    this.afAuth.signOut();
+  }
 
   ngOnInit(): void {
   }
