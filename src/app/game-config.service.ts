cbc56d97af3b3c8d3dcec76257e363dca3b38141
R. Alex Hofer <me@rofer.me>
2020-07-05 21:24:42 -0400

    Actually add game config files.

---
+++ src/app/game-config.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+import { PlayfieldConfig, MonsterConfig, TowerConfig, GameConfig } from './game-config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GameConfigService {
+
+  config: GameConfig = {
+    playfield: {
+      width: 10,
+      height: 10,
+      monsterEnter: { row:0, col: 0 },
+      monsterExit: { row:9, col: 0 },
+    },
+    monsters: [],
+    towers: [],
+  }
+  constructor() { }
+}
---
+++ src/app/game-config.ts
@@ -0,0 +1,31 @@
+export interface CellPos { row: number, col: number }
+
+export interface PlayfieldConfig {
+  width: number,
+  height: number,
+  monsterEnter: CellPos,
+  monsterExit: CellPos,
+}
+
+export interface MonsterConfig {
+  id: number,
+  name: string,
+  health: number,
+  speed: number,
+  bounty: number,
+}
+
+export interface TowerConfig {
+  id: number,
+  name: string,
+  cost: number,
+  firingRate: number,
+  range: number,
+  damage: number,
+}
+
+export interface GameConfig {
+  playfield: PlayfieldConfig,
+  monsters: MonsterConfig[],
+  towers: TowerConfig[],
+}
