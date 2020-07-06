5e48d259dc21db51dd34bcb74449e60b8778c0ca
R. Alex Hofer <me@rofer.me>
2020-07-06 19:55:59 -0400

    Start of Konva integration for gameplay rendering.

--- src/app/towers/towers.component.ts
+++ src/app/towers/towers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import Konva from 'konva';
 
 import { User } from '../user';
 import { BackendService } from '../backend.service';
@@ -21,6 +22,31 @@ export class TowersComponent implements OnInit {
     private backend: BackendService,
   ) { }
 
+  setupKonva() {
+    // Basic konva test
+    let stage = new Konva.Stage({
+      container: 'canvasDiv',
+      width: 500,
+      height: 500
+    });
+    let layer = new Konva.Layer();
+    stage.add(layer);
+
+    let box = new Konva.Rect({
+        x: 50,
+        y: 50,
+        width: 100,
+        height: 50,
+        fill: '#00D2FF',
+        stroke: 'black',
+        strokeWidth: 4,
+        draggable: true
+    });
+    layer.add(box);
+
+    layer.draw();
+  }
+
   ngOnInit(): void {
     this.getUser();
   }
@@ -28,7 +54,10 @@ export class TowersComponent implements OnInit {
   getUser(): void {
     this.username = this.route.snapshot.paramMap.get('username');
     if(this.username) {
-      this.backend.getUser(this.username).then(user => this.user = user);
+      this.backend.getUser(this.username).then(user => {
+        this.user = user;
+        this.setupKonva();
+      });
     }
   }
 
