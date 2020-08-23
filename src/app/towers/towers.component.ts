1fcb099d9b5e3ddc548d37e2a6dd25eed2d5f750
R. Alex Hofer <me@rofer.me>
2020-08-23 13:57:01 -0400

    Switch to using a User stream for the User in TowersComponent.

--- src/app/towers/towers.component.ts
+++ src/app/towers/towers.component.ts
@@ -4,7 +4,7 @@ import Konva from 'konva';
 import { Observable, EMPTY } from 'rxjs';
 
 import { User } from '../user';
-import { BackendService } from '../backend.service';
+import { UserService } from '../user.service';
 import { BattlegroundState } from '../battleground-state';
 import { BattlegroundStateService } from '../battleground-state.service';
 
@@ -15,13 +15,13 @@ import { BattlegroundStateService } from '../battleground-state.service';
   styleUrls: ['./towers.component.css']
 })
 export class TowersComponent implements OnInit {
-  public user: User | null = null;
+  public user$: Observable<User> = EMPTY;
   public errorMsg: string | null = null;
   public battlegroundState$: Observable<BattlegroundState> = EMPTY;
 
   constructor(
     private route: ActivatedRoute,
-    private backend: BackendService,
+    private userService: UserService,
     private bgStateService: BattlegroundStateService,
   ) { }
 
@@ -38,13 +38,7 @@ export class TowersComponent implements OnInit {
     const username = this.route.snapshot.paramMap.get('username');
 
     if(username) {
-      this.backend.getUser(username)
-        .then(user => {
-          this.user = user;
-          if (user === null) {
-            this.setError('Could not find user ' + username + '.');
-          }
-        });
+      this.user$ = this.userService.getUser(username);
       this.battlegroundState$ = this.bgStateService.getBattlegroundState(username);
     } else {
       this.setError('No username provided.');
---
+++ src/app/user.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { SseService } from './sse.service';
+import { User } from './user';
+import { environment } from '../environments/environment';
+import * as decoders from './decode';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private sseService: SseService) { }
+
+  getUser(username: string): Observable<User> {
+    return this.sseService
+      .getServerSentEvent(environment.serverAddress + '/userStream/' + username)
+      .pipe(map(resp => {
+        const respEvent = resp as MessageEvent;
+        const data = JSON.parse(respEvent.data);
+        console.log("Recieved new user state:");
+        console.log(data);
+        let decoded = decoders.user.decode(data);
+        if (decoded.isOk()) {
+          return decoded.value;
+        } else {
+          throw new Error('Failed to decode User: ' + decoded.error);
+        }
+      }));
+  }
+}
