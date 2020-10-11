7487e270c7e4fcca8432076609c5cef9dcfa2b36
R. Alex Hofer <me@rofer.me>
2020-10-11 12:36:22 -0400

    Fix tests for BattleLayerRenderer.

--- src/app/battle-layer-renderer/battle-layer-renderer.component.spec.ts
+++ src/app/battle-layer-renderer/battle-layer-renderer.component.spec.ts
@@ -1,12 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import Konva from 'konva';
 
 import { BattleLayerRendererComponent } from './battle-layer-renderer.component';
+import { mockGameConfig } from '../mock-game-config';
 
 describe('BattleLayerRendererComponent', () => {
   let component: BattleLayerRendererComponent;
   let fixture: ComponentFixture<BattleLayerRendererComponent>;
+  let stageSpy: jasmine.SpyObj<Konva.Stage>;
 
   beforeEach(async(() => {
+    stageSpy = jasmine.createSpyObj('Konva.Stage', ['add']);
+
     TestBed.configureTestingModule({
       declarations: [ BattleLayerRendererComponent ]
     })
@@ -16,6 +21,10 @@ describe('BattleLayerRendererComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BattleLayerRendererComponent);
     component = fixture.componentInstance;
+    // Set mandatory inputs.
+    component.stage = stageSpy;
+    component.gameConfig = mockGameConfig;
+
     fixture.detectChanges();
   });
 
