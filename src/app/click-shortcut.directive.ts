dcd93c5d3ecba9defc060feafabb4f1d418f2b44
R. Alex Hofer <me@rofer.me>
2020-10-14 21:29:51 -0400

    Add shortcut for "Add to wave" button. Add more checks to shortcut.

--- src/app/click-shortcut.directive.ts
+++ src/app/click-shortcut.directive.ts
@@ -1,14 +1,20 @@
-import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appClickShortcut]'
 })
-export class ClickShortcutDirective {
+export class ClickShortcutDirective implements OnInit {
   @Input() shortcutKey: string = '';
 
   constructor(private el: ElementRef) {
     if (!('disabled' in this.el.nativeElement)) {
-      console.warn("Click shortcut attached to element without a disabled property.");
+      console.warn('Click shortcut attached to element without a disabled property.');
+    }
+  }
+
+  ngOnInit() {
+    if (this.shortcutKey === '') {
+      console.warn('Shortcut set without shortcutKey.');
     }
   }
 
