ea028d55895d9b3fb634443684dbb93ddc216370
R. Alex Hofer <me@rofer.me>
2020-11-08 12:23:25 -0500

    Make leaderboard refresh and handle updates to logged in user.

--- src/app/leaderboard/leaderboard.component.ts
+++ src/app/leaderboard/leaderboard.component.ts
@@ -1,8 +1,12 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { Observable, EMPTY } from 'rxjs';
+import { Observable, EMPTY, timer, combineLatest } from 'rxjs';
+import { map, expand, mergeMap } from 'rxjs/operators';
 
+import { LoggedInUser } from '../logged-in-user';
 import { User } from '../user'
 import { BackendService } from '../backend.service'
+
+const REFRESH_MS = 10000; // Refresh every 10s
 
 @Component({
   selector: 'app-leaderboard',
@@ -11,13 +15,25 @@ import { BackendService } from '../backend.service'
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LeaderboardComponent {
-  users$: Observable<User[]> = EMPTY;
   displayedColumns = [ 'rank', 'name', 'accumulatedGold', 'goldPerMinute' ];
-  console = console;
 
   constructor(
-    public backend: BackendService
-  ) {
-    this.users$ = backend.getUsers();
-  }
+    private backend: BackendService
+  ) { }
+
+  private readonly polledUsers$: Observable<User[]> =
+    this.backend.getUsers().pipe(
+      expand(_ => timer(REFRESH_MS).pipe(
+        mergeMap(_ => this.backend.getUsers())
+      ))
+    );
+
+  public readonly usersRowData$: Observable<(User & {loggedIn: boolean})[]> =
+    combineLatest(this.polledUsers$, this.backend.getLoggedInUser()).pipe(
+      map(([users, loggedInUser]: [User[], LoggedInUser | undefined]) =>
+        users.map((user: User) =>
+          Object.assign({}, user, { loggedIn: loggedInUser !== undefined && loggedInUser.matches(user) })
+        )
+      )
+    );
 }
