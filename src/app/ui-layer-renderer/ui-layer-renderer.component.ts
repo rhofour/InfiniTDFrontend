8ffd7cf095419b8d42a236b0470162ebba1ac064
R. Alex Hofer <me@rofer.me>
2020-11-08 22:35:55 -0500

    Add animation of firing rate and projectileSpeed when selecting towers.

--- src/app/ui-layer-renderer/ui-layer-renderer.component.ts
+++ src/app/ui-layer-renderer/ui-layer-renderer.component.ts
@@ -16,6 +16,7 @@ export class UiLayerRendererComponent extends BaseLayerRendererComponent impleme
   private rows = 0;
   private cols = 0;
   private subscription: Subscription;
+  private ringAnim: Konva.Animation | undefined = undefined;
   @Input() gameConfig!: GameConfig;
 
   constructor(
@@ -25,6 +26,9 @@ export class UiLayerRendererComponent extends BaseLayerRendererComponent impleme
 
     this.subscription = this.selectionService.getSelection().subscribe((newSelection) => {
       this.selection = newSelection;
+      if (this.ringAnim) {
+        this.ringAnim.stop();
+      }
       this.render();
     });
   }
@@ -55,10 +59,11 @@ export class UiLayerRendererComponent extends BaseLayerRendererComponent impleme
       this.layer.add(selectionBox);
 
       if (this.selection.gridTower) {
+        const gridTower = this.selection.gridTower;
         let outerRangeCircle = new Konva.Circle({
           x: (this.selection.grid.col + 0.5) * this.cellSize_,
           y: (this.selection.grid.row + 0.5) * this.cellSize_,
-          radius: this.selection.gridTower.range / 100 * this.cellSize_,
+          radius: gridTower.range / 100 * this.cellSize_,
           fillEnabled: false,
           stroke: 'black',
           strokeWidth: this.cellSize_ * 0.05,
@@ -68,13 +73,51 @@ export class UiLayerRendererComponent extends BaseLayerRendererComponent impleme
         let innerRangeCircle = new Konva.Circle({
           x: (this.selection.grid.col + 0.5) * this.cellSize_,
           y: (this.selection.grid.row + 0.5) * this.cellSize_,
-          radius: this.selection.gridTower.range / 100 * this.cellSize_,
+          radius: gridTower.range / 100 * this.cellSize_,
           fillEnabled: false,
           stroke: 'lime',
           strokeWidth: this.cellSize_ * 0.025,
           shadowEnabled: false,
         });
         this.layer.add(innerRangeCircle);
+
+        if (gridTower.firingRate > 0) {
+          const firingPeriod = 1 / gridTower.firingRate;
+          const firingDuration = gridTower.range / gridTower.projectileSpeed / 100;
+          const numRings = Math.ceil(firingDuration / firingPeriod);
+          let rings: Konva.Circle[] = [];
+          for (let i = 0; i < numRings; i++) {
+            let ring = new Konva.Circle({
+              x: (this.selection.grid.col + 0.5) * this.cellSize_,
+              y: (this.selection.grid.row + 0.5) * this.cellSize_,
+              radius: 0,
+              fillEnabled: false,
+              stroke: 'red',
+              strokeWidth: this.cellSize_ * 0.025,
+              shadowEnabled: false,
+            });
+            rings.push(ring);
+            this.layer.add(ring);
+          }
+          if (this.ringAnim) {
+            this.ringAnim.stop();
+          }
+          this.ringAnim = new Konva.Animation((frame: any | undefined) => {
+            if (frame === undefined) {
+              return;
+            }
+            for (let i = 0; i < numRings; i++) {
+              const modTime = (firingPeriod * i + (frame.time / 1000)) % (firingPeriod * numRings);
+              const radius = modTime / firingDuration;
+              if (radius > 1.0 || (frame.time / 1000) < firingPeriod * i) {
+                rings[i].radius(0);
+              } else {
+                rings[i].radius(gridTower.range / 100 * this.cellSize_ * radius);
+              }
+            }
+          }, this.layer);
+          this.ringAnim.start();
+        }
       }
     }
 
